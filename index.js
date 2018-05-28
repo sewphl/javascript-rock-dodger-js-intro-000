298142410645cb49bc024d00d06c724725181c7a
sharon <sharonoptsout@gmail.com>
2018-05-28 20:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   ROCKS.forEach(function(rock) {rock.remove();});
-  document.removeEventListener('keydown', moveDodger;
+  document.removeEventListener('keydown', moveDodger);
   START.innerHTML = 'Play again?';
   START.style.display = 'inline';
   return alert('YOU LOSE!');
